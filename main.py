54a36bd5d51bfab84d1f86aa620d6781c35c91e1
gplantard <52867789+gplantard@users.noreply.github.com>
2021-11-13 15:04:58 +0100

    Génération de la page d'Exemple
    Se base sur les modules importés. Affiche le retour des foncions sample des modules commancant par "ex". possibilité de limiter seulement à une liste de module.

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
-import generate2nd
-import generateTermEsSpe
+#import generate2nd
+#import generateTermEsSpe
+#import automatisme
 
-generate2nd.run()
+import pyperclip
+
+import exMatrice
+import exEquation
+import exDroite
+
+#generate2nd.run()
 #generateTermEsSpe.run()
+
+modulesList = globals()
+
+def getSamples(modulesNames = None):
+    global modulesList
+    sampleDocument = ""
+    for mod in modulesList.keys():
+        if mod.startswith("ex") and (modulesNames == None or (modulesNames != None and mod in modulesNames)):
+            sampleDocument += "\\section {Module " + mod + "}\n"
+            sampleDocument += "\\begin{itemize}\n"
+            for function, result in modulesList[mod].samples().items():
+                sampleDocument +=  f"\\item {function} :\\\\ \n{result}\n"
+            sampleDocument += "\\end{itemize}\n"
+    print (sampleDocument)
+
+    pyperclip.copy(sampleDocument)
+
+    return sampleDocument
+
+getSamples()
+#getSamples(["exDroite"])
