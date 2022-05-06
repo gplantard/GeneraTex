9e6ed859ce8738b851a178a7cd5b14f8f92daed0
gplantard <gregory.plantard@ac-orleans-tours.fr>
2022-05-06 11:43:42 +0200

    Correction d'affichage de la primitive

--- Trinome.py
+++ Trinome.py
@@ -1,7 +1,9 @@
+from multiprocessing.spawn import import_main_path
 from sys import stderr
 from math import sqrt
 
-from Tools import wellTyped
+from Tools import wellTyped, getSignedString
+from random import choice, randint
 
 class Trinome:
     class Forme:
@@ -27,12 +29,12 @@ class Trinome:
     def _getX1(self):
         if self.delta < 0 :
             return None
-        return wellTyped((-self.b - sqrt(self.delta))/(2*self.a))
+        return wellTyped( (-(self.b - sqrt(self.delta)) ) / (2*self.a) )
     
     def _getX2(self):
         if self.delta < 0 :
             return None
-        return wellTyped((-self.b + sqrt(self.delta))/(2*self.a))
+        return wellTyped((-(self.b + sqrt(self.delta)))/(2*self.a))
 
     delta = property(_getDelta)
     x1 = property(_getX1)
@@ -59,16 +61,16 @@ class Trinome:
                 result += "x"
             if  self.x1 != 0:
                 result += "(x "
-                if self.x1 > 0:
+                if self.x1 < 0:
                     result += "+"
-                result += str(self.x1) + ")"
+                result += str(-self.x1) + ")"
             if self.x1 == self.x2:
                 result += "^{2}"
             elif  self.x2 != 0:
                 result += "(x "
-                if self.x2 > 0:
+                if self.x2 < 0:
                     result += "+"
-                result += str(self.x2) + ")"
+                result += str(-self.x2) + ")"
                         
         else : # default : if mode == Trinome.Forme.DEVELOPPE:
             result = ""
@@ -95,4 +97,53 @@ class Trinome:
                     result += " + "
                 result += str(self.c)
         return result
+        
+    def getPrimitive(self, d = None):
+        primitive = ""
+        if self.a % 3 == 0:
+            if self.a == 3:
+                A = ""
+            elif self.a == -3:
+                A = "-"
+            else:
+                A = str(self.a//3)
+        else:
+            A = "\\dfrac{"+str(self.a)+"}{3}"
+        primitive += f"{A} x^3"
+        
+        if self.b != 0:
+            if self.b % 2 == 0:
+                if self.b == 2:
+                    B = "+"
+                elif self.b == -2:
+                    B = "-"
+                else:
+                    B = getSignedString(self.b//2)
+            else:
+                B = "\\dfrac{"+str(abs(self.b))+"}{2}"
+                if self.b > 0:
+                    B = " + " + B
+                elif self.b < 0:
+                    B = " - " + B
+            primitive += f"{B} x^2"
+
+        if self.c != 0:
+            if self.c == 1:
+                C = ""
+            elif self.c == -1:
+                C = "-"
+            else:
+                C = getSignedString(self.c)
+            primitive += f"{C} x"
+
+        if d == None:
+            d = randint(1,20) * choice([-1,1])
+            if d != 0:
+                D = getSignedString(d)
+            else:
+                D = ""
+        primitive += D
+
+        return primitive
+        
         
--- test.py
+++ test.py
@@ -8,3 +8,4 @@ f = Trinome.fromFactor(-3,-2,2)
 
 print (f.toString())
 print (f.toString(Trinome.Forme.FACTORISEE))
+print(f.getPrimitive())
